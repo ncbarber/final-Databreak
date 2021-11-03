6a66f38b5528bcfcecc0dc08da41ce07ffca15f6
Nathaniel Barber` <ncbarber@bsu.edu>
2021-11-03 14:20:40 -0400

    Add basic handling of collision with collectable

--- project/src/Level/Level.gd
+++ project/src/Level/Level.gd
@@ -20,12 +20,28 @@ func _make_player() -> void:
 	player.position = SPAWN_POSITION
 	call_deferred("add_child", player)
 
+
 func _make_USB() -> void:
 	usb = load("res://src/Collectables/Data/USB.tscn").instance()
+	var _connection := usb.connect("body_entered", self, "_on_USB_Entered", [usb])
 	usb.position = USB_POSITION
 	call_deferred("add_child", usb)
 
+
 func _make_Floppy_Disk() -> void:
 	floppy = load("res://src/Collectables/Data/FloppyDisk.tscn").instance()
+	var _connection := floppy.connect("body_entered", self, "_on_Floppy_Entered", [floppy])
 	floppy.position = FLOPPY_POSITION
 	call_deferred("add_child", floppy)
+
+
+func _on_USB_Entered(body, _usb):
+	if body == player:
+		_usb.queue_free()
+		#Make note that data was collected
+
+
+func _on_Floppy_Entered(body, _floppy):
+	if body == player:
+		_floppy.queue_free()
+		#Make note that data was collected
