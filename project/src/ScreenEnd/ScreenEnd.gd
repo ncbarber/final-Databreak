28013bee52a57da88c9e4f21b4744eae7fa6acce
DuncanKlam <duncan.klemm141@gmail.com>
2021-11-04 10:48:33 -0400

    Force style guide compliance

--- project/src/ScreenEnd/ScreenEnd.gd
+++ project/src/ScreenEnd/ScreenEnd.gd
@@ -1,12 +1,12 @@
 extends Control
 
 
-func _ready():
+func _ready() -> void:
 	$BinaryParticle.visible = true
 	$BinaryParticleInverse.visible = true
 
 
-func _on_RestartButton_pressed():
+func _on_RestartButton_pressed() -> void:
 	var _ignored := get_tree().change_scene("res://src/ScreenStart/ScreenStart.tscn")
 	$BinaryParticle.visible = false
 	$BinaryParticleInverse.visible = false
