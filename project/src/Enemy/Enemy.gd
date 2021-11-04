90ae5f315b2e600b54124b1d6cd4d5c75625de59
Nathaniel <ncbarber11@gmail.com>
2021-11-04 00:08:28 -0400

    Implement the enemy detection and movement

--- project/src/Enemy/Enemy.gd
+++ project/src/Enemy/Enemy.gd
@@ -1,37 +1,33 @@
 extends KinematicBody2D
 
+signal player_hit 
+
 
 var run_speed := 30
 var gravity := 400
 var velocity := Vector2()
 var is_moving_left := false
+var direction = 1
 
+
+var player : KinematicBody2D
 
 
 func _ready():
-	var randomNumber : RandomNumberGenerator = RandomNumberGenerator.new() 
-	randomNumber.randomize()
-	$PatrolTimer.wait_time=randomNumber.randi_range(2,7)
+	if direction == 1:
+		$Sprite.flip_h = false
 
 
-func _process(_delta) -> void:
-	velocity.x = 0
-	print($PatrolTimer.time_left)
-	if $PatrolTimer.time_left<=0.1:
-		is_moving_left = !is_moving_left
-		$PatrolTimer.start()
-	if is_moving_left == true:
-		velocity.x -= run_speed
-		
-		
-		
-	if is_moving_left == false:
-		velocity.x += run_speed
-		
 func _physics_process(delta) -> void:
+	if $PatrolTimer.time_left == 0:
+		$PatrolTimer.start()
+		direction = direction * -1
+		$Sprite.flip_h = !$Sprite.flip_h
+	
 	velocity.y += gravity * delta
+	velocity.x = run_speed * direction
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 
 
-func _on_Light_body_entered(_body):
-	var _game_over := get_tree().change_scene("res://src/ScreenEnd/ScreenEnd.tscn")
+func _on_Light_body_entered(body):
+	emit_signal("player_hit", body)
--- project/src/Level/Level.gd
+++ project/src/Level/Level.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
 const SPAWN_POSITION := Vector2(496,460)
+const ENEMY_POSITION := Vector2(1284,338)
 const USB_POSITION := Vector2(8, 380)
 const FLOPPY_POSITION := Vector2(100, 380)
 
 var player : KinematicBody2D
+var enemy : KinematicBody2D
 var usb : Area2D
 var floppy : Area2D
 
 
 func _ready():
 	_make_player()
+	_make_enemy()
 	_make_USB()
 	_make_Floppy_Disk()
 
@@ -19,6 +22,12 @@ func _make_player() -> void:
 	player = load("res://src/Player/Player.tscn").instance()
 	player.position = SPAWN_POSITION
 	call_deferred("add_child", player)
+	
+func _make_enemy() -> void:
+	enemy = load("res://src/Enemy/Enemy.tscn").instance()
+	var _connection = enemy.connect("player_hit", self, "register_hit")
+	enemy.position = ENEMY_POSITION
+	call_deferred("add_child", enemy)
 
 
 func _make_USB() -> void:
@@ -33,6 +42,11 @@ func _make_Floppy_Disk() -> void:
 	var _connection := floppy.connect("body_entered", self, "_on_Floppy_Entered", [floppy])
 	floppy.position = FLOPPY_POSITION
 	call_deferred("add_child", floppy)
+
+
+func register_hit(body):
+	if body == player:
+		var _game_over := get_tree().change_scene("res://src/ScreenEnd/ScreenEnd.tscn")
 
 
 func _on_USB_Entered(body, _usb):
