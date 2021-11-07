cc5ca5f1f38bc35eb75c51022916926fbcb81c9e
Nathaniel <ncbarber11@gmail.com>
2021-11-07 12:41:07 -0500

    Edit Player.gd for get_inputs verbage

--- project/Player/Player.gd
+++ project/Player/Player.gd
@@ -9,7 +9,7 @@ var is_jumping := false
 var lives_remaining := 3
 
 
-func set_inputs() -> void:
+func _set_inputs() -> void:
 	velocity.x = 0
 	var right := Input.is_action_pressed('move_right')
 	var left := Input.is_action_pressed('move_left')
@@ -39,7 +39,7 @@ func set_inputs() -> void:
 
 
 func _physics_process(delta) -> void:
-	set_inputs()
+	_set_inputs()
 	velocity.y += gravity * delta
 	if is_jumping and is_on_floor() or is_jumping and is_on_ceiling():
 		is_jumping = false
