cc2bb2fada3fa4e80f07d02a1f2351236bc04ce8
Nathaniel <ncbarber11@gmail.com>
2021-12-09 02:52:40 -0500

    Add new peices to the tutorial fix merge issues on level

--- project/Tutorial/Tutorial.gd
+++ project/Tutorial/Tutorial.gd
@@ -26,15 +26,15 @@ func _on_SpawnArea_body_entered(body):
 		is_door_open = false
 
 
-func _on_DoorArea_body_exited(body):
+func _on_DoorArea_body_exited(_body):
 	is_door_open = true
 
 
-func _on_SpawnArea_body_exited(body):
+func _on_SpawnArea_body_exited(_body):
 	is_door_open = true
 
 
-func _on_Door2Area_body_entered(body):
+func _on_Door2Area_body_entered(_body):
 	if(startup):
 		startup=false
 	else:
