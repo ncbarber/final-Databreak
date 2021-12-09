8e7b0d4e70a0c6afba4980397e68f0a980d3556d
DuncanKlam <duncan.klemm141@gmail.com>
2021-12-08 22:22:04 -0500

    Blink works

--- project/Collectables/Abilities/Blink/Blink.gd
+++ project/Collectables/Abilities/Blink/Blink.gd
@@ -4,4 +4,3 @@ extends Area2D
 func _on_Blink_body_entered(_body) -> void:
 	RoomGlobals.ability_set('blink')
 	queue_free()
-
--- project/Player/SignalManager.gd
+++ project/Player/SignalManager.gd
@@ -19,13 +19,21 @@ signal handle_floppy
 signal handle_usb
 
 
-func _ready() ->void:
+signal blink
+signal blink_over
+signal enemy_blink
+signal enemy_blink_over
+
+
+func _ready() -> void:
 	var _connectionFloppy = connect('floppy_entered', self, '_on_floppy_entered')
 	var _connectionUSB = connect('usb_entered', self, '_on_usb_entered')
 	var _connectionGameOver = connect('game_over', self, '_on_game_over')
 	var _connectionUnlock = connect('unlock', self, '_on_unlock')
 	var _connectionInvis = connect("invisible", self, '_handle_invisible')
 	var _connectionVis = connect("visible", self, '_handle_visible')
+	var _connectionBlink = connect("blink", self, '_handle_blink')
+	var _connectionBlinkOver = connect("blink_over", self, '_handle_blink_over')
 	emit_signal('floppy_entered')
 	emit_signal('usb_entered')
 	emit_signal('game_over')
@@ -56,3 +64,9 @@ func _handle_invisible() -> void:
 
 func _handle_visible() -> void:
 	emit_signal("player_visible")
+
+func _handle_blink() -> void:
+	emit_signal('enemy_blink')
+
+func _handle_blink_over() -> void:
+	emit_signal('enemy_blink_over')
